171ef8a1750b75183769f6e9a3fe39dadd5465a9
万心雨 <547054575@qq.com>
2021-03-16 21:47:21 +0800

    2.HW-CE1 Commit

--- src/main/java/com/WanXinyu/week2/demo/HelloWordServlet.java
+++ src/main/java/com/WanXinyu/week2/demo/HelloWordServlet.java
@@ -5,14 +5,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.Date;
 public class HelloWordServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         PrintWriter writer = response.getWriter();
-        writer.println("Hello client !!!");
+        writer.println("Name:Wan Xinyu\n");
+        writer.println("ID:2019211001000902\n");
+        Date d = new Date();
+        String s = null;
+        writer.println("Date and Time " +d);
     }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response){
 
     }
